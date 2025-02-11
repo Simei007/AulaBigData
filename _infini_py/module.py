782dcb84876ba3dd065f9197d181298020b2feab
Simei007 <simeicondutor@gmail.com>
2025-02-11 20:31:47 -0300

    Adicionando código do projeto AulaBigData

--- _infini_py/module.py
+++ _infini_py/module.py
@@ -1,3 +1,7 @@
+# para fazer o commit para o github
+# git commit -m "Adicionando código do projeto AulaBigData"
+# para fazer o push para o github
+# git push origin main
 import pandas as pd
 df1 = pd.DataFrame({'lkey': ['foo', 'bar', 'baz', 'foo'], 'value': [1, 2, 3, 5]})
 df2 = pd.DataFrame({'rkey': ['foo', 'bar', 'baz', 'foo'], 'value': [5, 6, 7, 8]})
